cb2ea9268b8ebb7fc063a31bc9af86c4994c976f
chenlongxiang <chenlongxiang@indemind.cn>
2018-10-30 23:36:02 +0800

    delete

--- ModuleInfo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
-export QT_QPA_PLATFORM_PLUGIN_PATH=./plugins
-export QT_QPA_FONTDIR=./lib/fonts
-
-./ModuleInfo
